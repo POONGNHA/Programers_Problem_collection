58af0d1d70fb45555232ab409c1ef37fa1513f61
POONGNHA <105198445+POONGNHA@users.noreply.github.com>
2022-05-24 13:55:08 +0900

    Add files via upload

---
+++ Practice_Kth_Number.java
@@ -0,0 +1,34 @@
+class Swap{
+	int temp;
+	
+	public void swap(int x, int y) {
+	temp = x;
+	x = y;
+	y = temp;
+	}
+}
+
+class Solution {
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer = {};
+        return answer;
+    }
+}
+
+class Practice_Kth_Number{
+	public static void main(String[] args) {
+//		args[0] = [1, 5, 2, 6, 3, 7, 4];
+//		args[1]=  [[2, 5, 3], [4, 4, 1], [1, 7, 3]];
+//		Accounting a1 = new Accounting();
+		
+		String a1 = args[0];
+		String a2 = args[1];
+		
+		Solution s1 = new Solution();
+		
+		System.out.println(s1);
+		
+		
+		
+	}
+}
